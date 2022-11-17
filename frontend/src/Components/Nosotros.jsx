0ee2617e11e8c291ae1c40d4a94a854e18c5419e
FabianLopezChamorro <lopezmf@uninorte.edu.co>
2022-11-17 15:29:53 -0500

    Modificar el componente Nosotros

--- frontend/src/Components/Nosotros.jsx
+++ frontend/src/Components/Nosotros.jsx
@@ -5,7 +5,7 @@ function Nosotros() {
         <section className="page-section" id="about">
             <div className="container">
                 <div className="text-center">
-                    <h2 className="section-heading text-uppercase">About</h2>
+                    <h2 className="section-heading text-uppercase">Nosotros</h2>
                     <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                 </div>
                 <ul className="timeline">
@@ -14,7 +14,7 @@ function Nosotros() {
                         <div className="timeline-panel">
                             <div className="timeline-heading">
                                 <h4>2009-2011</h4>
-                                <h4 className="subheading">Our Humble Beginnings</h4>
+                                <h4 className="subheading">Nuestros Inicios</h4>
                             </div>
                             <div className="timeline-body"><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
                         </div>
@@ -24,7 +24,7 @@ function Nosotros() {
                         <div className="timeline-panel">
                             <div className="timeline-heading">
                                 <h4>March 2011</h4>
-                                <h4 className="subheading">An Agency is Born</h4>
+                                <h4 className="subheading">ToDo</h4>
                             </div>
                             <div className="timeline-body"><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
                         </div>
@@ -34,7 +34,7 @@ function Nosotros() {
                         <div className="timeline-panel">
                             <div className="timeline-heading">
                                 <h4>December 2015</h4>
-                                <h4 className="subheading">Transition to Full Service</h4>
+                                <h4 className="subheading">ToDo</h4>
                             </div>
                             <div className="timeline-body"><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
                         </div>
@@ -44,7 +44,7 @@ function Nosotros() {
                         <div className="timeline-panel">
                             <div className="timeline-heading">
                                 <h4>July 2020</h4>
-                                <h4 className="subheading">Phase Two Expansion</h4>
+                                <h4 className="subheading">ToDo</h4>
                             </div>
                             <div className="timeline-body"><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
                         </div>
@@ -52,11 +52,11 @@ function Nosotros() {
                     <li className="timeline-inverted">
                         <div className="timeline-image">
                             <h4>
-                                Be Part
+                                !Sé parte
                                 <br />
-                                Of Our
+                                De Nuestra
                                 <br />
-                                Story!
+                                Historia!
                             </h4>
                         </div>
                     </li>
