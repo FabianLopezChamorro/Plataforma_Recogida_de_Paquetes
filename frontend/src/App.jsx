7db369c0c388ffe5869196b1e209281b94ed121b
FabianLopezChamorro <lopezmf@uninorte.edu.co>
2022-11-17 14:31:14 -0500

    Probar el funcionamiento de Header en App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+//import './App.css'
 
 import NavBar from './Components/NavBar'
+import Header from './Components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <div >
       <NavBar />
+      <Header />
       <h1>ToDo</h1>
     </div>
   )
