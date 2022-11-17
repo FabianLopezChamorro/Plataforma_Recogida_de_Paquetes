cfb3bc3b4981aefd0e872ef2e9d16d48b3dd63f2
FabianLopezChamorro <lopezmf@uninorte.edu.co>
2022-11-17 14:18:01 -0500

    Modificar desplegable en NavBar

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -12,11 +12,11 @@ function NavBar() {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarResponsive">
                         <ul className="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-                            <li className="nav-item"><a className="nav-link" href="#services">Services</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#portfolio">Portfolio</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#team">Team</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#services">Rastrear</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#portfolio">Cotizar</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#about">Nosotros</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#team">Equipo</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#contact">Contacto</a></li>
                         </ul>
                     </div>
                 </div>
