0bcb88f1f647bf5f0073af50644664aa5c1c46ac
FabianLopezChamorro <lopezmf@uninorte.edu.co>
2022-11-17 14:14:07 -0500

    Ajustar color de fondo de NavBar

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark" id="mainNav">
                 <div className="container">
                     <a className="navbar-brand" href="#page-top"><img src="assets/img/navbar-logo.svg" alt="..." /></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
