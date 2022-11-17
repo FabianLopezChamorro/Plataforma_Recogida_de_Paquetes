ed5da532db11b4e0ad193806e89ebd3c5d6bc215
FabianLopezChamorro <lopezmf@uninorte.edu.co>
2022-11-17 14:54:34 -0500

    Probar el funcionamiento del componente Recoger

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import reactLogo from './assets/react.svg'
 
 import NavBar from './Components/NavBar'
 import Header from './Components/Header'
+import Recoger from './Components/Recoger'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <div >
       <NavBar />
       <Header />
+      <Recoger />
       <h1>ToDo</h1>
     </div>
   )
