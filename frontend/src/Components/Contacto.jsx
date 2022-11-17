0ebd665f24384dbe241eff78241f3ea75f0fadbc
FabianLopezChamorro <lopezmf@uninorte.edu.co>
2022-11-17 15:21:54 -0500

    Modificar el componente Contacto

--- frontend/src/Components/Contacto.jsx
+++ frontend/src/Components/Contacto.jsx
@@ -5,7 +5,7 @@ function Contacto() {
         <section className="page-section" id="contact">
             <div className="container">
                 <div className="text-center">
-                    <h2 className="section-heading text-uppercase">Contact Us</h2>
+                    <h2 className="section-heading text-uppercase">Contactanos</h2>
                     <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                 </div>
                 {/* * * * * * * * * * * * * * * **/}
@@ -20,26 +20,26 @@ function Contacto() {
                         <div className="col-md-6">
                             <div className="form-group">
                                 {/* Name input*/}
-                                <input className="form-control" id="name" type="text" placeholder="Your Name *" data-sb-validations="required" />
-                                <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                                <input className="form-control" id="name" type="text" placeholder="Nombre *" data-sb-validations="required" />
+                                <div className="invalid-feedback" data-sb-feedback="name:required">Se require nombre.</div>
                             </div>
                             <div className="form-group">
                                 {/* Email address input*/}
-                                <input className="form-control" id="email" type="email" placeholder="Your Email *" data-sb-validations="required,email" />
-                                <div className="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-                                <div className="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
+                                <input className="form-control" id="email" type="email" placeholder="Email *" data-sb-validations="required,email" />
+                                <div className="invalid-feedback" data-sb-feedback="email:required">Se require Email.</div>
+                                <div className="invalid-feedback" data-sb-feedback="email:email">Email no valido.</div>
                             </div>
                             <div className="form-group mb-md-0">
                                 {/* Phone number input*/}
-                                <input className="form-control" id="phone" type="tel" placeholder="Your Phone *" data-sb-validations="required" />
-                                <div className="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
+                                <input className="form-control" id="phone" type="tel" placeholder="Celular *" data-sb-validations="required" />
+                                <div className="invalid-feedback" data-sb-feedback="phone:required">Se require un número de celular.</div>
                             </div>
                         </div>
                         <div className="col-md-6">
                             <div className="form-group form-group-textarea mb-md-0">
                                 {/* Message input*/}
-                                <textarea className="form-control" id="message" placeholder="Your Message *" data-sb-validations="required"></textarea>
-                                <div className="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
+                                <textarea className="form-control" id="message" placeholder="Dejanos tu mensaje *" data-sb-validations="required"></textarea>
+                                <div className="invalid-feedback" data-sb-feedback="message:required">Se requiere mensaje.</div>
                             </div>
                         </div>
                     </div>
@@ -49,7 +49,7 @@ function Contacto() {
                     {/* has successfully submitted*/}
                     <div className="d-none" id="submitSuccessMessage">
                         <div className="text-center text-white mb-3">
-                            <div className="fw-bolder">Form submission successful!</div>
+                            <div className="fw-bolder">Mensaje enviado correctamente!</div>
                             To activate this form, sign up at
                             <br />
                             <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
@@ -61,7 +61,7 @@ function Contacto() {
                     {/* an error submitting the form*/}
                     <div className="d-none" id="submitErrorMessage"><div className="text-center text-danger mb-3">Error sending message!</div></div>
                     {/* Submit Button*/}
-                    <div className="text-center"><button className="btn btn-primary btn-xl text-uppercase disabled" id="submitButton" type="submit">Send Message</button></div>
+                    <div className="text-center"><button className="btn btn-primary btn-xl text-uppercase disabled" id="submitButton" type="submit">Enviar Mensaje</button></div>
                 </form>
             </div>
         </section>
